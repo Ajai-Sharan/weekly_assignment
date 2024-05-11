ac7f735d6c7b1d6add39afb0b53a8ff0fb7d7ec8
Ajai-Sharan <ajaisharan2020@gmail.com>
2024-05-11 13:23:46 +0530

    Added productController Testcase

---
+++ src/test/java/com/example/productservicesst/Controllers/ProductControllerTest.java
@@ -0,0 +1,24 @@
+package com.example.productservicesst.Controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void getAllProducts() {
+    }
+
+    @Test
+    void createNewProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+}
